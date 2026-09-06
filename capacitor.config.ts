2ea9345490c41b3b4f269e06f8db6d780c753247
carlosandre250317-del <carlosandre250317@gmail.com>
2026-09-06 14:38:45 -0300

    Add files via upload

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.eletricistapro.app',
   appName: 'Eletricista Pro',
-  webDir: '.',
+  webDir: "www",
   bundledWebRuntime: false,
   android: {
     backgroundColor: '#0A0A0A'
